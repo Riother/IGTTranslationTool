a28334f1f5293ac968792eb633ba5316ef94c751
laynekawahara <lkawahara@student.neumont.edu>
2015-10-14 15:36:23 -0600

    Client Obj

---
+++ IGTLocalizer/IGTLocalizer/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGTLocalizer
+{
+    public class Client
+    {
+        public Dictionary<String, String> textVariables { get; set; }
+
+        public Client() {
+            this.textVariables = new Dictionary<string, string>();
+        }
+
+        public Client(Dictionary<String, String> textVariables) {
+            this.textVariables = textVariables;
+        }
+
+        public bool AddVar(String key, String value){
+            bool valueUpdated = textVariables.Contains(new KeyValuePair<String, String>(key, value));
+            this.textVariables.Add(key, value);
+            return valueUpdated;
+        }
+
+
+    }
+}
