83963ba58654c9daf4fb8137bb9185411814ef57
Wardell James <wjames@student.neumont.edu>
2015-10-14 15:15:44 -0600

    Testing

---
+++ IGTLocalizer/IGTLocalizer/Model/TranslationCaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.API;
+using Google.API.Translate;
+namespace IGTLocalizer.Model
+{
+    class TranslationCaller
+    {
+       void test()
+        {
+            GoogleClient c = new TranslateClient();
+            c
+        }
+    }
+}
