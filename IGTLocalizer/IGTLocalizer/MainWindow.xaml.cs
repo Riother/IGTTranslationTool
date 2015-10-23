1f5f0a5b21bba367dfe4a6edb0b5801ddc448582
Alfredo De Leon <bored1118@gmail.com>
2015-10-23 16:16:16 -0600

    Button inside of AddLanguage user control now trigger the event from the main window

--- IGTLocalizer/IGTLocalizer/MainWindow.xaml.cs
+++ IGTLocalizer/IGTLocalizer/MainWindow.xaml.cs
@@ -94,6 +94,34 @@ namespace IGTLocalizer
             //fileEditor.Text = fileContentObject.ToString();
         }
 
+        private void CanTranslateFile(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void TranslateFile(object sender, ExecutedRoutedEventArgs e)
+        {
+            foreach(string p in properties)
+            {
+                fileContentObject["default"][p] =
+                    translator.TranslateLine(fileContentObject["default"][p].ToString(), startingLangCode, toTranslateLang.ls.selectLang);
+
+                JSONValue eValue = new JSONValue(false);
+                eValue.myValue.Text = fileContentObject["default"][p].ToString();
+                StkEditableValues.Children.Add(eValue);
+            }
+            //foreach(string client in clients)
+            //{
+            //    foreach(string prop in properties)
+            //    {
+            //        fileContentObject[client][prop] = 
+            //            translator.TranslateLine(fileContentObject[client][prop].ToString(), startingLangCode, translatedLangCode);
+            //    }
+            //}
+
+            //fileEditor.Text = fileContentObject.ToString();
+        }
+
         private void AddNewUser(Object sender, EventArgs e)
         {
             AddCustomer addCustID = new AddCustomer();
